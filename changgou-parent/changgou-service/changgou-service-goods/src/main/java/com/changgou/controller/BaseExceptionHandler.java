3f87deeb2aea877428d38deff23d38dc905db21e
WaWaWasmile <59128547+WaWaWasmile@users.noreply.github.com>
2020-07-27 21:18:52 +0800

    FastDFS实现文件上传下载功能

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/controller/BaseExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.changgou.controller;
+
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class BaseExceptionHandler {
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        return new Result(false, StatusCode.ERROR,"出错啦！",e.getMessage());
+    }
+}
